81e2822b5f106c217bd757bae6aefd572648f3e5
longjj <luojj26@mail2.sysu.edu.cn>
2017-02-15 22:05:28 +0800

    command unused icons

--- scripts/iconsInstall.sh
+++ scripts/iconsInstall.sh
@@ -6,12 +6,12 @@ apt-get install dalisha-icons
 
 # Numix
 
-apt-add-repository ppa:numix/ppa
-apt-get update
-apt-get install numix-icon-theme-circle
+# apt-add-repository ppa:numix/ppa
+# apt-get update
+# apt-get install numix-icon-theme-circle
 
 # SQUARE 2.0 ICON PACK
 
-add-apt-repository ppa:noobslab/icons2
-apt-get update
-apt-get install square-icons
+# add-apt-repository ppa:noobslab/icons2
+# apt-get update
+# apt-get install square-icons
